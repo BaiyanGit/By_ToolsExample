31b136531d909dc0f93d278332cd96af204848b9
bygithub <774726599@qq.com>
2026-02-26 09:03:37 +0800

    no message

--- By_ToolsExample/Assets/Demos/示例-UI曲面叠加滚动/无限滚动模块化/辅助脚本/OnGuiDesc.cs
+++ By_ToolsExample/Assets/Demos/示例-UI曲面叠加滚动/无限滚动模块化/辅助脚本/OnGuiDesc.cs
@@ -15,12 +15,14 @@ namespace Demos.示例_UI曲面叠加滚动.Scripts
 
         private GUIStyle _style;
 
-        private const string Desc = "<b><color=red><size=30> UI曲面叠加滚动 </size></color></b>\n\n" +
-                                    "未使用UGUI控件ScrollRect实现的UI曲面叠加滚动效果。\n\n" +
+        private const string Desc = "<b><color=red><size=40> 不使用ScrollRect实现的UI叠加滚动 </size></color></b>\n\n" +
                                     "支持：\n" + "无限循环滚动、曲面滚动、曲面缩放、曲面颜色变化。\n\n" +
                                     "操作：\n" +
                                     "鼠标滚轮滚动、左右箭头键切换曲面、拖拽滚动、点击Item滚动居中。\n\n" +
-                                    "【曲面UI滚动】 ：需要在Image、RawImage、Text等组件上挂载 <color=green>ByCurvedUI</color> 材质，并设置曲面参数。\n";
+                                    "曲面UI滚动：\n" +
+                                    "在Image、RawImage、Text上挂载 <color=green>ByCurvedUI</color> 材质，并设置曲面参数\n" +
+                                    "<color=red>注意：</color>相机一定是Camera模式且不能是正交！！！\n" +
+                                    "(使用曲面后，鼠标点击Item触发会有聚焦错误问题，暂时无解)";
 
         private void OnGUI()
         {
@@ -36,7 +38,7 @@ namespace Demos.示例_UI曲面叠加滚动.Scripts
             };
 
             // 使用 GUILayout 来适配内容大小
-            GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+            GUILayout.BeginArea(new Rect(10, 10, Screen.width, Screen.height));
             _style.fontSize         = _fontSize;
             _style.normal.textColor = _fontColor;
 
